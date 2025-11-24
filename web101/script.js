c10ab881a449d8cd2749f1e9afd0df32b4379ac2
Nick10062004 <narakantasupawit@gmail.com>
2025-11-24 11:58:37 +0700

    Initial commit: add basic web files

--- web101/script.js
+++ web101/script.js
@@ -1,3 +1,8 @@
 function showMessage() {
   alert("Hello from JavaScript!");
 }
+const heading = document.querySelector("h1");
+
+function changeTitle() {
+  heading.textContent = "You changed the title!";
+}
